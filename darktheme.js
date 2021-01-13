0a81818d6591670bcfa9fd08013681ee6f1b3d57
msm79 <mohammadmarandi89@gmail.com>
2021-01-13 13:19:33 +0330

    fix dark mode

--- darktheme.js
+++ darktheme.js
@@ -1,32 +1,68 @@
+var element = document.body;
+var header = document.getElementById('headerd');
+var layer = document.getElementById('layer1');
+var article = document.getElementsByClassName('article');
+var p = document.getElementsByClassName('paraglight');
+var title = document.getElementsByClassName('title');
+var search = document.getElementById('search');
+var links = document.getElementsByClassName('alink');
+var themeModeText = document.getElementById('theme-mode-text');
+
 function dark() {
-   var element = document.body;
-   var header = document.getElementById('headerd');
-   var layer = document.getElementById('layer1');
-   var article = document.getElementsByClassName('article');
-   var p = document.getElementsByClassName('paraglight');
-   var title = document.getElementsByClassName('title');
-   var search = document.getElementById('search');
-   var links = document.getElementsByClassName('alink');
-   element.classList.toggle("dark-mode");
-   header.classList.toggle('headerdark');
-   layer.classList.toggle('layer-dark');
-   search.classList.toggle('search-dark');
+   element.classList.add("dark-mode");
+   header.classList.add('headerdark');
+   layer.classList.add('layer-dark');
+   search.classList.add('search-dark');
    for (var i = 0; i < links.length; i++) {
-     links[i].classList.toggle('alinks');
+     links[i].classList.add('alinks');
    }
    for (var i = 0; i < article.length; i++) {
-     article[i].classList.toggle('article-dark');
+     article[i].classList.add('article-dark');
    }
    for (var i = 0; i < title.length; i++) {
-     title[i].classList.toggle('title-dark');
+     title[i].classList.add('title-dark');
    }
    for (var i = 0; i < p.length; i++) {
-     p[i].classList.toggle('parag');
+     p[i].classList.add('parag');
    }
+   themeModeText.innerHTML = "حالت شب";
+   sessionStorage.setItem("mode" , "dark");
 }
 
-// var today = new Date();
-// var time = today.getHours();
-// if (time > 17 || time < 5) {
-//   dark();
-// }
+function light() {
+  element.classList.remove("dark-mode");
+  header.classList.remove('headerdark');
+  layer.classList.remove('layer-dark');
+  search.classList.remove('search-dark');
+  for (var i = 0; i < links.length; i++) {
+    links[i].classList.remove('alinks');
+  }
+  for (var i = 0; i < article.length; i++) {
+    article[i].classList.remove('article-dark');
+  }
+  for (var i = 0; i < title.length; i++) {
+    title[i].classList.remove('title-dark');
+  }
+  for (var i = 0; i < p.length; i++) {
+    p[i].classList.remove('parag');
+  }
+  themeModeText.innerHTML = "حالت روز";
+  sessionStorage.setItem("mode" , "light");
+}
+
+function handleClick() {
+  const mode = sessionStorage.getItem("mode");
+  if (mode === "dark") {
+    light();
+  } else {
+    dark();
+  }
+}
+
+var today = new Date();
+var time = today.getHours();
+if (time >= 22 || time <= 7) {
+  dark();
+} else {
+  light();
+}
